3d22f17720bc8b1bd48f3d41b73a0a75dc050782
marek890 <73061993+marek890@users.noreply.github.com>
2022-03-29 09:33:04 +0200

    Add files via upload

---
+++ login_script.php
@@ -0,0 +1,32 @@
+<?php
+require_once('connection.php');
+$isEmpty = false;
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['psw']);
+$hash = md5($_POST["psw"]);
+
+if (empty($username)) {
+    $isEmpty = true;
+}
+if (empty($password)) {
+    $isEmpty = true;
+}
+
+if ($isEmpty == true) {
+    echo "Nieco si nezadal" . "<br>";
+}
+
+if ($isEmpty == false) {
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$hash'";
+    $results = mysqli_query($conn, $query);
+    
+    if (mysqli_num_rows($results) == 1) {
+
+        echo "Uspesne si sa prihlasil" . "<br>";
+    }
+    else {
+        echo "Zadal si nespravne meno alebo heslo" . "<br>";
+    }
+}
+?>
